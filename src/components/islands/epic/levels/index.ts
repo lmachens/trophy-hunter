647c6dd72ff2c1c9069135a9468ac186b42fb046
lmachens <leon.machens@googlemail.com>
2020-05-16 12:57:20 +0200

    Add epic island markers

---
+++ src/components/islands/epic/levels/index.ts
@@ -0,0 +1,2 @@
+export { default as lvl1 } from './lvl1';
+export { default as lvl2 } from './lvl2';
---
+++ src/components/islands/epic/levels/lvl2.ts
@@ -0,0 +1,13 @@
+import { Level } from '../../../levels/types';
+import { EpicIcon, EpicMarker } from '../../../levels/epic';
+
+const lvl2: Level = {
+  island: 'epic',
+  name: 'lvl2',
+  title: 'Epic island Lvl. 2',
+  Icon: EpicIcon,
+  Marker: EpicMarker,
+  trophies: []
+};
+
+export default lvl2;

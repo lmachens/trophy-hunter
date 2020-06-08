51ea30f0dd902417f99cc7f6c127e3ff5e764e79
lmachens <leon.machens@googlemail.com>
2020-06-08 19:27:55 +0200

    Add favourite button

--- src/components/icons/FavoritesFilter.tsx
+++ src/components/icons/FavoritesFilter.tsx
@@ -1,19 +1,30 @@
-import { FC } from 'react';
+import { FC, SVGProps } from 'react';
+import styled from '@emotion/styled';
 
-const FavoritesFilter: FC = () => {
+interface FavoritesFilterProps extends SVGProps<SVGSVGElement> {
+  active?: boolean;
+}
+
+const SVG = styled.svg<FavoritesFilterProps>`
+  fill: ${(props) => (props.active ? '#77777A' : '#1F1F1F')};
+  stroke: #77777a;
+
+  &:hover {
+    fill: #3f3e43;
+  }
+`;
+
+const FavoritesFilter: FC<FavoritesFilterProps> = (props) => {
   return (
-    <svg
+    <SVG
       width="20"
       height="20"
       viewBox="0 0 20 20"
-      fill="none"
       xmlns="http://www.w3.org/2000/svg"
+      {...props}
     >
-      <path
-        d="M10 2L11.7961 7.52786H17.6085L12.9062 10.9443L14.7023 16.4721L10 13.0557L5.29772 16.4721L7.09383 10.9443L2.39155 7.52786H8.20389L10 2Z"
-        fill="#77777A"
-      />
-    </svg>
+      <path d="M10 2L11.7961 7.52786H17.6085L12.9062 10.9443L14.7023 16.4721L10 13.0557L5.29772 16.4721L7.09383 10.9443L2.39155 7.52786H8.20389L10 2Z" />
+    </SVG>
   );
 };
 
--- src/components/trophies/TrophyListItem.tsx
+++ src/components/trophies/TrophyListItem.tsx
@@ -2,6 +2,8 @@ import { FC } from 'react';
 import styled from '@emotion/styled';
 import { Trophy } from './types';
 import { useTrophyProgress } from '../../contexts/account';
+import FavoritesFilter from '../icons/FavoritesFilter';
+import Grow from '../common/Grow';
 
 interface ListItemProps {
   borderless?: boolean;
@@ -9,13 +11,17 @@ interface ListItemProps {
 }
 const ListItem = styled.div<ListItemProps>`
   background: #2b2a30;
-  height: 86px;
+  padding: 10px;
   margin-bottom: 4px;
   display: flex;
   border-top: ${(props) => (props.borderless ? 'none' : '1px solid #3f3e43')};
   background-image: url(${process.env.PUBLIC_DIR}/notifications/${(props) => props.island}.png);
   background-position: bottom right;
   background-repeat: no-repeat;
+
+  &:hover {
+    background: #3e3d42;
+  }
 `;
 
 interface TrophyListItemProps extends ListItemProps {
@@ -28,6 +34,11 @@ const Progress = styled.div`
   margin: 6px 10px 6px 6px;
   flex-shrink: 0;
   width: 12px;
+`;
+
+const Favorite = styled(FavoritesFilter)`
+  flex-shrink: 0;
+  cursor: pointer;
 `;
 
 const TrophyListItem: FC<TrophyListItemProps> = ({
@@ -49,10 +60,11 @@ const TrophyListItem: FC<TrophyListItemProps> = ({
       <Progress>
         <trophy.ProgressIcon progress={trophyProgress} />
       </Progress>
-      <div>
+      <Grow>
         <h3>{trophy.title}</h3>
         <p>{trophy.description}</p>
-      </div>
+      </Grow>
+      {borderless && <Favorite />}
     </ListItem>
   );
 };

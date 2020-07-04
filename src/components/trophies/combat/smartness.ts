57138ac695260c77c4977267870e810ef5140ca8
lmachens <leon.machens@googlemail.com>
2020-07-04 22:11:16 +0200

    Add smartness live check

--- src/components/trophies/combat/smartness.ts
+++ src/components/trophies/combat/smartness.ts
@@ -17,6 +17,29 @@ const smartness: Trophy = {
 
     return (hasKillingSpree + hasTenAssists + hasLessThanFiveDeaths) / 3;
   },
+  checkLive: ({ events, account }) => {
+    const deaths = events.filter(
+      (event) =>
+        event.EventName === 'ChampionKill' &&
+        event.VictimName === account.summoner.name
+    ).length;
+    if (deaths > 5) {
+      return 0;
+    }
+
+    const killingSprees = events.filter(
+      (event) =>
+        event.EventName === 'Multikill' &&
+        event.KillerName === account.summoner.name
+    ).length;
+    const assists = events.filter(
+      (event) =>
+        event.EventName === 'ChampionKill' &&
+        event.Assisters.includes(account.summoner.name)
+    ).length;
+
+    return (Math.min(1, killingSprees) + Math.min(1, assists / 10)) / 2;
+  },
 };
 
 export default smartness;

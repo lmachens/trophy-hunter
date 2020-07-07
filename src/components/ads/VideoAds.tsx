2eef68141ddf49f4ce58974299c63c33bff526ab
lmachens <leon.machens@googlemail.com>
2020-07-07 09:55:24 +0200

    Add OwAds

--- src/components/ads/VideoAds.tsx
+++ src/components/ads/VideoAds.tsx
@@ -1,5 +1,7 @@
-import { FC } from 'react';
+import { FC, useEffect, useRef, useState } from 'react';
 import styled from '@emotion/styled';
+import overwolf from '../../api/overwolf';
+import { OwAd } from '../../../typings/owAds';
 
 const Container = styled.div`
   background: #2b2a30;
@@ -16,7 +18,74 @@ const Container = styled.div`
 `;
 
 const VideoAds: FC = () => {
-  return <Container />;
+  const containerRef = useRef(null);
+  const [owAd, setOwAd] = useState<OwAd>(null);
+
+  useEffect(() => {
+    const handleOwAdReady = () => {
+      if (typeof globalThis.OwAd === 'undefined') {
+        return;
+      }
+      console.log(`OwAd ready`);
+
+      const owAd: OwAd = new globalThis.OwAd(containerRef.current, {
+        size: { width: 400, height: 300 },
+      });
+
+      const handleInternalRendered = () => {
+        owAd.removeEventListener(
+          'ow_internal_rendered',
+          handleInternalRendered
+        );
+        console.log(`Ads ready`);
+
+        setOwAd(owAd);
+      };
+
+      owAd.addEventListener('ow_internal_rendered', handleInternalRendered);
+    };
+
+    const script = document.createElement('script');
+    script.src = 'https://content.overwolf.com/libs/ads/latest/owads.min.js';
+    script.async = true;
+    document.body.appendChild(script);
+
+    script.onload = handleOwAdReady;
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (!owAd) {
+      return;
+    }
+    const handleWindowStateChanged = (
+      state: overwolf.windows.WindowStateChangedEvent
+    ): void => {
+      if (state) {
+        // when state changes to minimized, call removeAd()
+        if (state.window_state === 'minimized') {
+          owAd.removeAd();
+        }
+        // when state changes from minimized to normal, call refreshAd()
+        else if (
+          state.window_previous_state === 'minimized' &&
+          state.window_state === 'normal'
+        ) {
+          owAd.refreshAd(null);
+        }
+      }
+    };
+    overwolf.windows.onStateChanged.addListener(handleWindowStateChanged);
+
+    return () => {
+      overwolf.windows.onStateChanged.removeListener(handleWindowStateChanged);
+    };
+  }, [owAd]);
+
+  return <Container ref={containerRef} />;
 };
 
 export default VideoAds;

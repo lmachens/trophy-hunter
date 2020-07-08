5ffe0496b458c947bcd2497c960630fbf0ecc0a1
lmachens <leon.machens@googlemail.com>
2020-07-08 09:39:04 +0200

    Update lifeAndDeath

--- src/components/trophies/teamwork/lifeAndDeath.ts
+++ src/components/trophies/teamwork/lifeAndDeath.ts
@@ -1,20 +1,23 @@
 import { Trophy } from '../types';
 import { getParticipantByAccount } from '../../../api/riot/helpers';
-import { findPerk } from '../../../api/accounts/helpers';
+import { findPerk, getTrophyProgress } from '../../../api/accounts/helpers';
 
 const lifeAndDeath: Trophy = {
   island: 'teamworkIsland',
   name: 'lifeAndDeath',
   level: 'teamwork3',
   title: 'Life And Death',
-  description: 'Deal and heal more than 1200 with grasp of the undying (rune).',
+  description:
+    'Deal and heal more than 2500 in total with grasp of the undying (rune).',
   category: 'teamwork',
+  maxProgress: 2500,
   checkProgress: ({ match, account }) => {
     const participant = getParticipantByAccount(match, account);
 
     const lifeAndDeath = findPerk(participant, 8437);
+    const trophyProgress = getTrophyProgress(account, 'lifeAndDeath');
 
-    return Number(lifeAndDeath.var1 >= 1200 && lifeAndDeath.var2 >= 1200);
+    return (lifeAndDeath.var1 + lifeAndDeath.var2) / 2500 + trophyProgress;
   },
 };
 

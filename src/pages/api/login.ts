45e85ca31b2d09be3dea8349a051e6dc1fb68466
lmachens <leon.machens@googlemail.com>
2020-09-14 11:17:34 +0200

    Fix authToken

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -79,7 +79,7 @@ export default applyMiddleware(
 
     res.setHeader(
       'Set-Cookie',
-      `authToken=${authToken};path=/;Max-Age=${ONE_YEAR / 1000};HttpOnly;${
+      `authToken=${authToken};path=/;Max-Age=${ONE_YEAR / 1000};HttpOnly${
         process.env.NODE_ENV === 'production' ? ';SameSite=None;Secure' : ''
       }`
     );

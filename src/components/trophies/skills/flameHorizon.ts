bcb36413ee54bbc2b6cd45a537325fadcd51c7bc
lmachens <leon.machens@googlemail.com>
2020-11-30 13:47:00 +0100

    ✏️Fix Flame Horizon title

--- src/components/trophies/skills/flameHorizon.ts
+++ src/components/trophies/skills/flameHorizon.ts
@@ -5,11 +5,12 @@ const flameHorizon: Trophy = {
   island: 'skillsIsland',
   name: 'flameHorizon',
   level: 'skills3',
-  title: 'Flame Breath',
+  title: 'Flame Horizon',
   description: 'Kill at least 100 more minions than your lane opponent.',
   category: 'skills',
   checkProgress: ({ match, participant }) => {
     const laneOpponent = getLaneOpponent(match.participants, participant);
+    console.log({ laneOpponent }, { participant });
     if (!laneOpponent) {
       return 0;
     }

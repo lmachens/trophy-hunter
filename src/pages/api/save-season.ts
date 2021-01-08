493b5e28f7e04427f05ea5973350dde58e882c61
lmachens <leon@machens.koeln>
2021-01-08 12:58:30 +0100

    🐛 Only save season accounts for accounts with games

--- src/pages/api/save-season.ts
+++ src/pages/api/save-season.ts
@@ -17,50 +17,56 @@ export default applyMiddleware(
       return res.status(400).send('Already saved');
     }
     await Accounts.find({}).forEach(async (account) => {
-      await SeasonAccounts.insertOne({
-        season: '10',
-        summoner: account.summoner,
-        islands: account.islands,
-        levels: account.levels,
-        trophies: account.trophies,
-        games: account.games,
-        lastGameIds: account.lastGameIds,
-        trophiesCompleted: account.trophiesCompleted,
-      });
-      const now = Date.now();
-      await Accounts.updateOne(
-        {
-          _id: account._id,
-        },
-        {
-          $set: {
-            islands: [
-              {
-                name: 'hub',
-                status: 'open',
-              },
-            ],
-            levels: [
-              'welcome',
-              'hubCombat',
-              'hubEpic',
-              'hubObjectives',
-              'hubSkills',
-              'hubSpecial',
-              'hubTeamwork',
-            ].map((name) => ({
-              name,
-              island: 'hub',
-              status: 'active',
-              unlockedAt: now,
-            })),
-            trophies: [],
-            games: 0,
-            favoriteTrophyNames: [],
-            trophiesCompleted: 0,
-          },
+      try {
+        if (account.games > 0) {
+          await SeasonAccounts.insertOne({
+            season: '10',
+            summoner: account.summoner,
+            islands: account.islands,
+            levels: account.levels,
+            trophies: account.trophies,
+            games: account.games,
+            lastGameIds: account.lastGameIds,
+            trophiesCompleted: account.trophiesCompleted,
+          });
         }
-      );
+        const now = Date.now();
+        await Accounts.updateOne(
+          {
+            _id: account._id,
+          },
+          {
+            $set: {
+              islands: [
+                {
+                  name: 'hub',
+                  status: 'open',
+                },
+              ],
+              levels: [
+                'welcome',
+                'hubCombat',
+                'hubEpic',
+                'hubObjectives',
+                'hubSkills',
+                'hubSpecial',
+                'hubTeamwork',
+              ].map((name) => ({
+                name,
+                island: 'hub',
+                status: 'active',
+                unlockedAt: now,
+              })),
+              trophies: [],
+              games: 0,
+              favoriteTrophyNames: [],
+              trophiesCompleted: 0,
+            },
+          }
+        );
+      } catch (error) {
+        console.error(account._id, error.message);
+      }
     });
     console.log('DONE');
     res.send('OK');

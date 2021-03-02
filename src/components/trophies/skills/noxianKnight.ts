d403bb1e328872b6362bba9a1d224b8d81f426de
lmachens <leon@machens.koeln>
2021-03-02 19:35:43 +0100

    ✨ [Noxian Knight] Add ARAM support

--- src/components/trophies/skills/noxianKnight.ts
+++ src/components/trophies/skills/noxianKnight.ts
@@ -1,13 +1,15 @@
 import { Trophy } from '../types';
 import { getParticipantKillsAndAssists } from '../../../api/riot/helpers';
+import { ARAM_HOWLING_ABYSS } from '../../../api/overwolf';
 
 const noxianKnight: Trophy = {
   island: 'skills',
   name: 'noxianKnight',
   level: 'skills2',
   title: 'Noxian Knight',
-  description: 'Be involved in 60% of your teams kills and achieve level 18.',
+  description: `Be involved in 60% of your teams kills and achieve level 18.\nARAM: 85%`,
   category: 'skills',
+  aramSupport: true,
   checkProgress: ({ match, events, participant }) => {
     const teamParticipantIds = match.participants
       .filter((other) => other.teamId === participant.teamId)
@@ -24,8 +26,10 @@ const noxianKnight: Trophy = {
         teamParticipantIds.includes(event.killerId)
     ).length;
 
+    const requiredRatio = match.queueId === ARAM_HOWLING_ABYSS ? 0.85 : 0.6;
     return Number(
-      killsAndAssists / teamkills >= 0.6 && participant.stats.champLevel >= 18
+      killsAndAssists / teamkills >= requiredRatio &&
+        participant.stats.champLevel >= 18
     );
   },
 };

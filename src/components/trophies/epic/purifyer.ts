10eb97f33e3d18f24808a21bd3551afb97ee750a
lmachens <leon@machens.koeln>
2021-03-02 19:55:22 +0100

    ✨ [Purifyer] Add ARAM support

--- src/components/trophies/epic/purifyer.ts
+++ src/components/trophies/epic/purifyer.ts
@@ -1,3 +1,4 @@
+import { ARAM_HOWLING_ABYSS } from '../../../api/overwolf';
 import { Trophy } from '../types';
 
 const purifyer: Trophy = {
@@ -5,17 +6,20 @@ const purifyer: Trophy = {
   name: 'purifyer',
   level: 'epic1',
   title: 'Purifyer',
-  description: 'Score at least 30 kills in a match.',
+  description: `Score at least 30 kills in a match.\nARAM: 32 kills`,
   category: 'epic',
-  checkProgress: ({ participant }) => {
-    return participant.stats.kills / 30;
+  aramSupport: true,
+  checkProgress: ({ participant, match }) => {
+    const requiredKills = match.queueId === ARAM_HOWLING_ABYSS ? 32 : 30;
+    return participant.stats.kills / requiredKills;
   },
-  checkLive: ({ allPlayers, account }) => {
+  checkLive: ({ allPlayers, account, gameData }) => {
     const accountPlayer = allPlayers.find(
       (player) => player.summonerName === account.summoner.name
     );
 
-    return accountPlayer.scores.kills / 30;
+    const requiredKills = gameData.gameMode === 'ARAM' ? 32 : 30;
+    return accountPlayer.scores.kills / requiredKills;
   },
 };
 

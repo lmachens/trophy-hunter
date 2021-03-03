c824595016a2e39a46356425a8ee28c7e51b3cd3
lmachens <leon@machens.koeln>
2021-03-03 14:44:51 +0100

    ✨ [The Elephant] Add ARAM support

--- src/components/trophies/teamwork/theElephant.ts
+++ src/components/trophies/teamwork/theElephant.ts
@@ -1,3 +1,4 @@
+import { ARAM_HOWLING_ABYSS } from '../../../api/overwolf';
 import { Trophy } from '../types';
 
 const theElephant: Trophy = {
@@ -5,17 +6,18 @@ const theElephant: Trophy = {
   name: 'theElephant',
   level: 'hubTeamwork',
   title: 'The Elephant',
-  description: 'Do not die for more than 20 minutes.',
+  description: `Do not die for more than 20 minutes.\nARAM: Ten minutes`,
   category: 'teamwork',
+  aramSupport: true,
   checkProgress: ({ match, participant }) => {
+    const requiredTimelimit = match.queueId === ARAM_HOWLING_ABYSS ? 600 : 1200;
     if (
       !participant.stats.longestTimeSpentLiving &&
-      match.gameDuration >= 1200
+      match.gameDuration >= requiredTimelimit
     ) {
       return 1;
     }
-
-    return participant.stats.longestTimeSpentLiving / 1200;
+    return participant.stats.longestTimeSpentLiving / requiredTimelimit;
   },
   checkLive: ({ events, gameData, account }) => {
     const deaths = events.filter(
@@ -23,12 +25,15 @@ const theElephant: Trophy = {
         event.EventName === 'ChampionKill' &&
         event.VictimName === account.summoner.name
     );
-
+    const requiredTimelimit = gameData.gameMode === 'ARAM' ? 600 : 1200;
     const lastDeath = deaths[deaths.length - 1];
-    if (!lastDeath && gameData.gameTime > 1200) {
+    if (!lastDeath && gameData.gameTime > requiredTimelimit) {
       return 1;
     }
-    if (lastDeath && gameData.gameTime - lastDeath.EventTime > 1200) {
+    if (
+      lastDeath &&
+      gameData.gameTime - lastDeath.EventTime > requiredTimelimit
+    ) {
       return 1;
     }
     return 0;

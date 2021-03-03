ebfa8141c16bceac9532e9bf92e9bca188221471
lmachens <leon@machens.koeln>
2021-03-03 13:35:29 +0100

    ✨ [Rageblade] Add ARAM support

--- src/components/trophies/epic/rageblade.ts
+++ src/components/trophies/epic/rageblade.ts
@@ -1,3 +1,4 @@
+import { ARAM_HOWLING_ABYSS } from '../../../api/overwolf';
 import { Trophy } from '../types';
 
 const rageblade: Trophy = {
@@ -5,10 +6,14 @@ const rageblade: Trophy = {
   name: 'rageblade',
   level: 'epic1',
   title: 'Rageblade',
-  description: 'Deal more than 75000 total damage to champions.',
+  description: `Deal more than 75000 total damage to champions.\nARAM: 80000 total damage`,
   category: 'epic',
-  checkProgress: ({ participant }) => {
-    return Number(participant.stats.totalDamageDealtToChampions >= 75000);
+  aramSupport: true,
+  checkProgress: ({ participant, match }) => {
+    const requiredDamage = match.queueId === ARAM_HOWLING_ABYSS ? 80000 : 75000;
+    return Number(
+      participant.stats.totalDamageDealtToChampions >= requiredDamage
+    );
   },
 };
 

48abae44052e2dc056dbebdfb570ed864f5008a5
lmachens <leon@machens.koeln>
2021-03-03 15:22:40 +0100

    ✨ [Unleash The Power] Add ARAM support

--- src/components/trophies/combat/unleashThePower.ts
+++ src/components/trophies/combat/unleashThePower.ts
@@ -1,14 +1,19 @@
 import { Trophy } from '../types';
 import { getLevelUps, getParticipantKills } from '../../../api/riot/helpers';
+import { ARAM_HOWLING_ABYSS } from '../../../api/overwolf';
 
 const unleashThePower: Trophy = {
   island: 'combat',
   name: 'unleashThePower',
   level: 'combat4',
   title: 'Unleash The Power',
-  description: 'Achieve a kill in the 30 seconds after you reached level six.',
+  description: `Achieve a kill in the 30 seconds after you reached level six.\nARAM: 20 seconds`,
   category: 'combat',
-  checkProgress: ({ events, participant }) => {
+  aramSupport: true,
+  checkProgress: ({ events, participant, match }) => {
+    const requiredTimelimt =
+      match.queueId === ARAM_HOWLING_ABYSS ? 20000 : 30000;
+
     const levelUps = getLevelUps(events, participant.participantId);
     const levelSix = levelUps[5];
     if (!levelSix) {
@@ -19,7 +24,7 @@ const unleashThePower: Trophy = {
     const hasKillInTimeframe = kills.some(
       (kill) =>
         kill.timestamp > levelSix.timestamp &&
-        kill.timestamp - 30000 < levelSix.timestamp
+        kill.timestamp - requiredTimelimt < levelSix.timestamp
     );
 
     return Number(hasKillInTimeframe);
@@ -33,12 +38,13 @@ const unleashThePower: Trophy = {
       return 0;
     }
 
+    const requiredTimelimt = gameData.gameMode === 'ARAM' ? 20 : 30;
     const hasKillInTimeframe = events.some(
       (event) =>
         event.EventName === 'ChampionKill' &&
         event.KillerName === account.summoner.name &&
         event.EventTime > trophyData.unleashThePower &&
-        event.EventTime + 30 < trophyData.unleashThePower
+        event.EventTime + requiredTimelimt < trophyData.unleashThePower
     );
 
     return Number(hasKillInTimeframe);

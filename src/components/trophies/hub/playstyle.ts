b45ba13811cc8bc85f8e2c8ef68d45452ea3121e
lmachens <leon@machens.koeln>
2021-03-22 15:27:09 +0100

    ✨ [New Born] Add ARAM support

--- src/components/trophies/hub/playstyle.ts
+++ src/components/trophies/hub/playstyle.ts
@@ -5,8 +5,10 @@ const playstyle: Trophy = {
   name: 'playstyle',
   level: 'welcome',
   title: 'New Born',
-  description: "Play one game of Summoner's Rift with the Trophy Hunter app.",
+  description:
+    "Play one game of Summoner's Rift or ARAM with the Trophy Hunter app.",
   category: 'hub',
+  aramSupport: true,
   checkProgress: () => 1,
 };
 

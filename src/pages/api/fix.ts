e3ac4c0116b4b3a9ca19bbe23c66b1159e3e4b23
lmachens <leon@machens.koeln>
2021-04-18 11:51:14 +0200

    🔒 Add new trophies to migration

--- src/pages/api/fix.ts
+++ src/pages/api/fix.ts
@@ -6,14 +6,7 @@ import {
 } from '../../api/utils/server/middleware';
 import { getAccountsCollection } from '../../api/accounts/server/collection';
 import { AccountTrophy } from '../../api/accounts';
-import {
-  celebrity,
-  famous,
-  friendly,
-  popular,
-  prominent,
-} from '../../components/trophies';
-import { startingTrophies } from '../../api/accounts/server';
+import { valentines, forTheVoid } from '../../components/trophies';
 import { trophyToAccountTrophy } from '../../api/accounts/server/functions';
 
 export default applyMiddleware(
@@ -23,37 +16,14 @@ export default applyMiddleware(
     await Accounts.find({}).forEach(async (account) => {
       const newTrophies: AccountTrophy[] = [];
 
-      startingTrophies.forEach((startingTrophy) => {
-        if (
-          !account.trophies.some(
-            (trophy) =>
-              trophy.name === startingTrophy.name &&
-              startingTrophy.name !== friendly.name
-          )
-        ) {
-          newTrophies.push(startingTrophy);
-        }
-      });
-      const hubSpecial = account.levels.find(
-        (level) => level.name === 'hubSpecial'
-      );
-      if (
-        hubSpecial?.status &&
-        !account.trophies.some((trophy) => trophy.name === friendly.name)
-      ) {
-        newTrophies.push(trophyToAccountTrophy(friendly));
-        if (hubSpecial.status === 'completed') {
-          hubSpecial.status = 'unlocked';
-        }
-      }
       const special1 = account.levels.find(
         (level) => level.name === 'special1'
       );
       if (
         special1 &&
-        !account.trophies.some((trophy) => trophy.name === popular.name)
+        !account.trophies.some((trophy) => trophy.name === forTheVoid.name)
       ) {
-        newTrophies.push(trophyToAccountTrophy(popular));
+        newTrophies.push(trophyToAccountTrophy(forTheVoid));
         if (special1.status === 'completed') {
           special1.status = 'unlocked';
         }
@@ -63,35 +33,11 @@ export default applyMiddleware(
       );
       if (
         special2 &&
-        !account.trophies.some((trophy) => trophy.name === prominent.name)
+        !account.trophies.some((trophy) => trophy.name === valentines.name)
       ) {
-        newTrophies.push(trophyToAccountTrophy(prominent));
+        newTrophies.push(trophyToAccountTrophy(valentines));
         if (special2.status === 'completed') {
           special2.status = 'unlocked';
-        }
-      }
-      const special3 = account.levels.find(
-        (level) => level.name === 'special3'
-      );
-      if (
-        special3 &&
-        !account.trophies.some((trophy) => trophy.name === famous.name)
-      ) {
-        newTrophies.push(trophyToAccountTrophy(famous));
-        if (special3.status === 'completed') {
-          special3.status = 'unlocked';
-        }
-      }
-      const special4 = account.levels.find(
-        (level) => level.name === 'special4'
-      );
-      if (
-        special4 &&
-        !account.trophies.some((trophy) => trophy.name === celebrity.name)
-      ) {
-        newTrophies.push(trophyToAccountTrophy(celebrity));
-        if (special4.status === 'completed') {
-          special4.status = 'unlocked';
         }
       }
 

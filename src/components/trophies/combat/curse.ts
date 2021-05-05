8c6e73a2e8ecbcadf064f6faad6e8d02bfacdf69
lmachens <leon@machens.koeln>
2021-05-05 10:34:24 +0200

    ⚡️ [Curse] Decrease time from 60 to 45

--- src/components/trophies/combat/curse.ts
+++ src/components/trophies/combat/curse.ts
@@ -1,14 +1,20 @@
+import { minutesToSeconds } from '../../../api/utils/dates';
 import { Trophy } from '../types';
+
+const REQUIRED_MINUTES = 45;
 
 const curse: Trophy = {
   island: 'combat',
   name: 'curse',
   level: 'combat7',
   title: 'Curse',
-  description: 'The total crowd control time that you dealt exceeds one hour.',
+  description: `The total crowd control time that you dealt exceeds ${REQUIRED_MINUTES} minutes.`,
   category: 'combat',
   checkProgress: ({ participant }) => {
-    return participant.stats.totalTimeCrowdControlDealt / 3600;
+    return (
+      participant.stats.totalTimeCrowdControlDealt /
+      minutesToSeconds(REQUIRED_MINUTES)
+    );
   },
 };
 
